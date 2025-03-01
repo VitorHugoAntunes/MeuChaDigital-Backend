39a902f99f8fa24a74e0036788d3d2a054a12d6a
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-01 13:38:12 -0300

    refactor: change slug formatting

--- dist/utils/formatSlug.js
+++ dist/utils/formatSlug.js
@@ -2,6 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.formatSlug = void 0;
 const formatSlug = (slug) => {
-    return slug.toLowerCase().replace(/ /g, "-");
+    return slug
+        .toLowerCase() // Converte para minúsculas
+        .normalize("NFD") // Normaliza caracteres Unicode (remove acentos)
+        .replace(/[\u0300-\u036f]/g, "") // Remove diacríticos (acentos)
+        .replace(/[^\w\s-]/g, "") // Remove caracteres especiais (exceto espaços e hifens)
+        .replace(/\s+/g, "-") // Substitui espaços por hifens
+        .replace(/-+/g, "-") // Remove múltiplos hifens consecutivos
+        .replace(/^-+|-+$/g, ""); // Remove hifens do início e do fim
 };
 exports.formatSlug = formatSlug;
--- src/utils/formatSlug.ts
+++ src/utils/formatSlug.ts
@@ -1,5 +1,12 @@
 const formatSlug = (slug: string) => {
-  return slug.toLowerCase().replace(/ /g, "-");
-}
+  return slug
+    .toLowerCase()
+    .normalize("NFD") // Normaliza caracteres Unicode
+    .replace(/[\u0300-\u036f]/g, "") // Remove (acentos)
+    .replace(/[^\w\s-]/g, "") // Remove caracteres especiais (exceto espaços e hifens)
+    .replace(/\s+/g, "-") // Substitui espaços por hifens
+    .replace(/-+/g, "-") // Remove múltiplos hifens consecutivos
+    .replace(/^-+|-+$/g, ""); // Remove hifens do início e do fim
+};
 
 export { formatSlug };
